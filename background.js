5c7df63b18013bf6bd45a7a5f77445c63c4eaf36
Logan Martel <Logan Martel>
2016-01-20 12:29:53 -0500

    get companies from Glassdoor API

--- background.js
+++ background.js
@@ -38,12 +38,43 @@ var getParams = function() {
   return promise;
 }
 
+var getCompanies = function(url, query, params) {
+  var results = [], deferreds = [];
+  var promise = $.Deferred();
+
+  query.forEach(function(entry){
+    params.q = entry;
+    deferreds.push(
+      $.ajax({
+        url: url,
+        type: "get",
+        data: params,
+        dataType: 'jsonp',
+        success: function(JSON) {results.push(JSON.response.employers[0])},
+        error: function(XMLHttpRequest, textStatus, errorThrown) { 
+          alert("Status: " + textStatus);
+        }        
+      })
+    );
+  });
+  
+  $.when.apply($, deferreds).then(function() {
+    promise.resolve(results);
+  });
+
+  return promise;
+}
+
 var showResults = function (query) {
   var url, el, i, cur;
   url = "http://api.glassdoor.com/api/api.htm";
   $.when(
     getParams()
   ).then(function(data) {
-    console.log(data);
+    $.when(
+      getCompanies(url, query, data)
+    ).then(function(companies) {
+        console.log(companies);
+    });
   });
 }
