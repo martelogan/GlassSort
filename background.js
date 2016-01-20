7ad24f25ac6466aafe828a3b2ae60fac5fe52c31
Logan Martel <Logan Martel>
2016-01-20 12:27:57 -0500

    gets params for api request

--- background.js
+++ background.js
@@ -6,6 +6,44 @@ $(document).ready(function() {
     });
 });
 
-var showResults = function(query) {
-  console.log(query);
+var getIp = function() {
+  var promise = $.Deferred();
+  $.ajax({
+    url: "http://ipinfo.io",
+    type: 'get',
+    dataType: 'jsonp',
+    success: function(result) {
+      promise.resolve(result.ip);
+    }
+  });
+  return promise;
+}
+
+var getParams = function() {
+  var promise = $.Deferred();
+  $.when(
+    getIp()
+  ).then(function(ip){
+      var data = ({
+        v : "1",
+        format : "json",
+        "t.p" : "51706",
+        "t.k" : "xBpFKWDKEM",
+        userip: ip,
+        useragent: navigator.userAgent,
+        action: "employers"
+      });
+      promise.resolve(data);
+  });
+  return promise;
+}
+
+var showResults = function (query) {
+  var url, el, i, cur;
+  url = "http://api.glassdoor.com/api/api.htm";
+  $.when(
+    getParams()
+  ).then(function(data) {
+    console.log(data);
+  });
 }
